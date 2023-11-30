6decec6271d30cc8f48263b2bffd58999a5a76e6
adamrisaldi <32086322+adamrisaldi@users.noreply.github.com>
2023-11-30 09:31:11 +0700

    Update index.js

--- Day7/soal3/index.js
+++ Day7/soal3/index.js
@@ -2,19 +2,12 @@ let kata = 'katam';
 let jumlahKata = kata.length;
 let a = '';
 
-// for (let i = 0; jumlahKata > i; jumlahKata--) {
-//     a += kata[jumlahKata-1];
-// } 
-
-// if (kata === a) {
-//     console.log(true);
-// }
 let result
 for (let i = 0; i<jumlahKata ; i++) {
         if (kata[i] !== kata[kata.length-1-i]) {
-            result =false
-            break
+            result = false;
+            break;
         }
-    result =true
+    result = true;
     }
-    console.log(result)
+    console.log(result);
